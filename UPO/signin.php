1c0cf4dbf20ee5ec580fe085d111d8bbb45f2894
Md. Rakib Ul Hasan Sefat <58886202+rakibulhasan-111@users.noreply.github.com>
2022-01-17 09:33:24 +0600

    Add files via upload

--- UPO/signin.php
+++ UPO/signin.php
@@ -1,89 +1,69 @@
 <?php
-// Initialize the session
 session_start();
  
-// Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: index.php");
     exit;
 }
  
-// Include config file
 include('config/db_connect.php');
  
-// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
  
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter username.";
     } else{
         $username = trim($_POST["username"]);
     }
     
-    // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
     
-    // Validate credentials
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
-            // Set parameters
             $param_username = $username;
             
-            // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Store result
                 mysqli_stmt_store_result($stmt);
                 
-                // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
                             session_start();
                             
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
+                            $_SESSION["username"] = $username;
                             
-                            // Redirect user to welcome page
+                            setcookie("user", $_SESSION["username"], time() + (86400 * 30), "/");
+                            
                             header("location: index.php");
                         } else{
-                            // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
                         }
                     }
                 } else{
-                    // Username doesn't exist, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
     
-    // Close connection
     mysqli_close($conn);
 }
 ?>
@@ -105,10 +85,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     <div class="wrapper">
     <div id="formContent">
-    <!-- Tabs Titles -->
     <h2 class="active"> Sign In </h2>
 
-    <!-- Icon -->
     <div>
     <script language = "javascript" type = "text/javascript">
         document.write("W E L C O M E")
@@ -119,7 +97,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       <img src="pic/logo.png" id="icon" alt="উ প সং হা র" />
     </div>
 
-    <!-- Login Form -->
     <?php 
         if(!empty($login_err)){
             echo '<div class="alert alert-danger">' . $login_err . '</div>';
